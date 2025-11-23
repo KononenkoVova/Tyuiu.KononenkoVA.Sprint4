2493553f02359c9ebac0377347f18b039e19f2cb
Владимир Кононенко <Tyuiu.KononenkoVA@outlook.com>
2025-11-23 10:59:13 +0300

    Task3

---
+++ Tyuiu.KononenkoVA.Sprint4.Task3.V5.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.KononenkoVA.Sprint4.Task3.V5.Lib
+{
+    public class DataService : ISprint4Task3V5
+    {
+        public int Calculate(int[,] array)
+        {
+            int sum = 0;
+            int rows = array.GetLength(0);
+
+            for (int i = 0; i < rows; i++)
+            {
+                sum += array[i, 0];
+            }
+
+            return sum;
+        }
+    }
+}
